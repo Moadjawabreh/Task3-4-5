0ef902390642659f8175600ba471759a63e0e547
Moad Jawabreh <moadjawabreh2001@gmail.com>
2023-12-03 16:47:54 +0300

    done

---
+++ Tasks/Task3/Task3/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Task1
+            //int[] arr1 = { 1, 7, 9, 45 };
+
+            //string[] arr2 = { "Str", "alex", "moh" };
+
+            //string[] arr3 = { "the", "fox", "over", "lazy", "dog" };
+
+            //Task2
+
+            //String[ ]  fruits = ["Tomato", "Banana", "Watermelon"]
+            //What the index of "Banana","Tomato" ? 
+            //ans : 1 , 0
+
+            //Task3
+
+            //string[] favoriteFoods = { "Pizza", "Sushi", "Chocolate", "Burgers", "Ice Cream" };
+            //string[] favoriteSports = { "Football", "Basketball", "Tennis" };
+            //string[] favoriteMovies = { "Inception", "The Shawshank Redemption", "The Matrix", "Pulp Fiction" };
+
+            //Console.WriteLine("Favorite Foods:");
+            //foreach (var food in favoriteFoods)
+            //{
+            //    Console.WriteLine(food);
+            //}
+
+            //Console.WriteLine("\nFavorite Sports:");
+            //foreach (var sport in favoriteSports)
+            //{
+            //    Console.WriteLine(sport);
+            //}
+
+            //Console.WriteLine("\nFavorite Movies:");
+            //foreach (var movie in favoriteMovies)
+            //{
+            //    Console.WriteLine(movie);
+            //}
+
+
+            //Task4
+
+            //string input=Console.ReadLine();
+            //string[] arr=(input.Split(','));
+            //int[] intArr = new int[arr.Length];
+            //int sum = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    intArr[i]= Convert.ToInt32(arr[i]);
+            //}
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    sum = sum + intArr[i];
+            //}
+            //Console.WriteLine(sum);
+
+            //Task5
+
+            //Console.Write("Enter the value of n: ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine($"The first {n} terms of odd numbers are:");
+
+            //int sum = 0;
+            //for (int i = 1; i <= 100; i += 2)
+            //{
+            //    Console.Write($"{i} ");
+            //    sum += i;
+            //    n--;
+
+            //    if (n == 0)
+            //        break;
+            //}
+
+            //Console.WriteLine($"\nThe sum of odd numbers is: {sum}");
+
+            //Task6
+
+            //string star = "*";
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine(star);
+            //    star = star+"*";
+            //}
+
+            //Task7
+
+            //int count = 1;
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    Console.WriteLine();
+            //    for (int j = 0; j <=i; j++)
+            //    {
+            //        Console.Write(" "+count);
+            //        count++;
+            //    }
+            //    Console.WriteLine();
+
+            //}
+
+
+        }
+    }
+}
---
+++ Tasks/Task4/Task4/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1
+            //Console.WriteLine("Input 10 numbers:");
+
+            //CalculateSumAndAverage(10);
+
+            //Task2
+            //Console.Write("Input number of terms: ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    Console.WriteLine($"Number is: {i} and cube of the {i} is: {Math.Pow(i, 3)}");
+            //}
+
+            //Task3
+
+            int[] years = { 1763, 1972, 1925, 1916, 1984, 1124, 1950, 2020 };
+
+            int[] result = GetYears(years);
+
+            Console.WriteLine("Years greater than 1950:");
+            foreach (var year in result)
+            {
+                Console.WriteLine(year);
+            }
+
+
+        }
+
+        static void GetYears(int[] years)
+        {
+            foreach (var item in years)
+            {
+                if (item>1950)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+
+
+
+
+        static void CalculateSumAndAverage(int count)
+        {
+            int sum = 0;
+
+            for (int i = 1; i <= count; i++)
+            {
+                Console.Write($"Number-{i}: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                sum += number;
+            }
+            double average = (double)sum / count;
+            Console.WriteLine($"sum {sum} , average {average}");
+
+        }
+    }
+}
